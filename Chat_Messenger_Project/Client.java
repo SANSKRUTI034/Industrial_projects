90c22f26c10d42c62bbd772e220f0144d154dc14
Sanskruti Pande <161758224+SANSKRUTI034@users.noreply.github.com>
2026-02-27 16:30:17 +0530

    Add files via upload

---
+++ Chat_Messenger_Project/Client.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+
+class ChatClientloop
+{
+    public static void main (String A[]) throws Exception
+    {
+
+
+
+        Socket sobj = new Socket("localhost", 5100);
+        System.out.println("Client gets connected with server succesfully");
+
+        PrintStream pobj = new PrintStream(sobj.getOutputStream());             
+        BufferedReader bobj1 = new BufferedReader(new InputStreamReader(sobj.getInputStream()));
+        BufferedReader bobj2 = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("------------------------------------------------");
+        System.out.println("Marvellous Client");
+        System.out.println("------------------------------------------------");
+
+        String str1 = null , str2 = null;
+        System.out.println("Enter message for server:");
+        while(true)
+{
+    str1 = bobj2.readLine();
+    pobj.println(str1);
+
+    if(str1.equals("END"))
+    {
+        break;
+    }
+
+    str2 = bobj1.readLine();
+    System.out.println("Server says: " + str2);
+    System.out.println("Enter message for server:");
+}
+
+sobj.close();    
+    }
+}
